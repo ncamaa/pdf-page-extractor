3ee24d2679c67ca8607665bd1e518c5438017965
Nadav Cohen <ncamaa@gmail.com>
2024-08-26 08:11:48 +0200

    message

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pdf-page-extractor',
+  base: '/pdf-page-extractor/',
   plugins: [react()],
   resolve: {
     alias: {
